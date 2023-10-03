cf6b5a207cb91c310b3b5130bc5b4bfb8d7956e6
Nikita Razlivanov <rnikitav@icloud.com>
2023-10-03 20:32:07 +0400

    Удалена регистрация пользователей. Удалено подтверждение email пользователя #2

--- README.md
+++ README.md
@@ -6,7 +6,6 @@
 
 ```shell
 composer install
-npm install
 php artisan migrate
 php artisan db:seed
 php artisan key:generate
--- routes/api/auth.php
+++ routes/api/auth.php
@@ -3,18 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::name('auth.')->prefix('auth')->middleware('throttle')->group(function () {
-    Route::post('registration', [
-        'uses' => '\App\Http\Controllers\Auth\AuthController@registration',
-        'as' => 'registration',
-    ]);
-    Route::post('confirm', [
-        'uses' => '\App\Http\Controllers\Auth\AuthController@confirm',
-        'as' => 'confirm',
-    ]);
-    Route::post('retry-activate', [
-        'uses' => '\App\Http\Controllers\Auth\AuthController@retryActivate',
-        'as' => 'retry-activate',
-    ]);
     Route::post('forgot', [
         'uses' => '\App\Http\Controllers\Auth\AuthController@forgot',
         'as' => 'forgot',
@@ -26,11 +14,6 @@ Route::name('auth.')->prefix('auth')->middleware('throttle')->group(function () 
     Route::post('token', [
         'uses' => '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken',
         'as' => 'token',
-        'middleware' => 'verified',
-    ]);
-    Route::post('check-email', [
-        'uses' => '\App\Http\Controllers\Auth\AuthController@checkEmail',
-        'as' => 'check-email',
     ]);
 });
 
