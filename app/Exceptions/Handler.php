6596e02ff0ae8292d0f11ba109b9242bfb24fd3a
Nikita Razlivanov <rnikitav@icloud.com>
2023-10-04 02:19:45 +0400

    some fixes #3

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,42 +6,17 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
     protected $levels = [
         //
     ];
 
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
-     */
     protected $dontReport = [
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
-
-    public function register()
-    {
-        $this->renderable(function (AuthUserNotVerifiedException $exception) {
-            return response()->json([
-                'message' => $exception->getMessage(),
-                'status_code' => 'not_verified'
-            ], AuthUserNotVerifiedException::$statusCode);
-        });
-    }
 }
